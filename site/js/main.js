343ce508f6f1d8d891e63a01ea6964747b972a93
itggot-linus-ohrn <linus.ohrn@elev.ga.ntig.se>
2019-04-26 13:42:20 +0200

    a

--- site/js/main.js
+++ site/js/main.js
@@ -2,8 +2,8 @@ let i = 0
 let boxes = document.querySelectorAll(".start")
 while(i < boxes.length){
     let box = boxes[i]
-    x_pos = Math.floor(Math.random() * 100)
-    y_pos = Math.floor(Math.random() * 100)
+    x_pos = Math.floor(Math.random() * 80)
+    y_pos = Math.floor(Math.random() * 80)
     box.style.left = x_pos+'%'
     box.style.top = y_pos+'%'
     i++;
